522d5cff12091df2986d8e8cb60020ca862b6d0c
Okan Arabaci <106684926+okanarabaci@users.noreply.github.com>
2023-01-30 19:53:45 +0100

    Include .esql files in copyBuildFilesTask()

--- src/main/groovy/com/ibm/gradle/plugins/ace/tasks/CopyBuildFilesTask.groovy
+++ src/main/groovy/com/ibm/gradle/plugins/ace/tasks/CopyBuildFilesTask.groovy
@@ -47,6 +47,7 @@ abstract class CopyBuildFilesTask extends Copy {
         include '**/*.msgflow'
         include '**/*.json'
         include '**/*.policyxml'
+        include '**/*.esql'
         include '**/.project'
 
         Task jarTask = project.tasks.findByName('jar')
